e1b2ac86015f878439e448c70e2bf79601bbf8f8
yiwei <yiwei@advmeds.com>
2018-12-19 16:02:23 +0800

    重新規劃 member頁面的Layout。 改用Recyclerview實作下面的 我的團購、我的最愛.....。

---
+++ app/src/main/java/ohyeah5566/ui/ui/fragment/BuyingFragment.java
@@ -0,0 +1,39 @@
+package ohyeah5566.ui.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import ohyeah5566.ui.R;
+
+/**
+ * Created by yiwei on 2018/12/18.
+ */
+public class BuyingFragment extends Fragment {
+
+
+    @BindView(R.id.recyclerview_item) RecyclerView recyclerviewItem;
+    Unbinder unbinder;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_buying, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
--- app/src/main/java/ohyeah5566/ui/ui/fragment/MemberFragment.java
+++ app/src/main/java/ohyeah5566/ui/ui/fragment/MemberFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +17,19 @@ import ohyeah5566.ui.R;
  * Created by yiwei on 2018/11/23.
  */
 public class MemberFragment extends Fragment{
-    @Nullable
+    private BuyingFragment buyingFragment;
+    private FragmentManager fragmentManager;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fragmentManager = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fl_mine_framelayout,new BuyingFragment());
+        fragmentTransaction.commit();
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_me, container, false);
